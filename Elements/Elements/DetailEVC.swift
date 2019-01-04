d3d11131448d567748e45397bc681be188afa5c4
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-01-04 13:43:34 -0500

    added my detail vc outlets and on action button and linkde them to the detail vc

--- Elements/Elements/DetailEVC.swift
+++ Elements/Elements/DetailEVC.swift
@@ -9,11 +9,22 @@
 import UIKit
 
 class DetailEVC: UIViewController {
+    @IBOutlet weak var largerElementImage: UIImageView!
+    @IBOutlet weak var elementSymbol: UILabel!
+    @IBOutlet weak var numberOfElementInThePT: UILabel!
+    @IBOutlet weak var atomicMassValue: UILabel!
+    @IBOutlet weak var meltingPointValue: UILabel!
+    @IBOutlet weak var boilingPointValue: UILabel!
+    @IBOutlet weak var elementWasDiscoveredBy: UILabel!
+    @IBOutlet weak var myFavoriteElement: UIBarButtonItem!
+    var iExpectAnElmentFromThePeriodicTable: PeriodicTableData!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
     }
-  
+    @IBAction func addThisElementToMyFavorites(_ sender: Any) {
+    }
+    
 }
