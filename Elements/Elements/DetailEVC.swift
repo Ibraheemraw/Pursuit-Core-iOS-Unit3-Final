8acd2b3d42f84aad488fa20428c4934b8795764d
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-01-04 10:51:01 -0500

    added two files on for the second view controller and the other for my custom tableview cell, gave them the appropiate custom classes. Refactored my first VCs name

---
+++ Elements/Elements/DetailEVC.swift
@@ -0,0 +1,19 @@
+//
+//  DetailEVC.swift
+//  Elements
+//
+//  Created by Ibraheem rawlinson on 1/4/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class DetailEVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+  
+}
---
+++ Elements/Elements/ElementTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ElementTableViewCell.swift
+//  Elements
+//
+//  Created by Ibraheem rawlinson on 1/4/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ElementTableViewCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        
+    }
+    
+}
