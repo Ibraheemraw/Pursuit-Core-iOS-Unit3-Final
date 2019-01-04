38800f56034d273d55786bd5d92fc070b342b46a
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-01-04 11:45:35 -0500

    added api client file with two static functions to search for data and add data to my favorites

---
+++ Elements/Elements/PeriodicTableApiClient.swift
@@ -0,0 +1,13 @@
+//
+//  PeriodicTableApiClient.swift
+//  Elements
+//
+//  Created by Ibraheem rawlinson on 1/4/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
+final class PeriodicTableApiClient {
+    static func searchForElementsInThePeriodicTable(){}
+    static func sendElementMyFavorites(){}
+}
